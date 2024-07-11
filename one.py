9c7363ba77c449ae2721bcdd3496daf380f6de88
Vishal-TK <vishalwarghade01@gmail.com>
2024-07-11 15:03:16 +0530

    Pipeline

--- one.py
+++ one.py
@@ -1,1 +1,32 @@
-print("Hello World")
+pipeline {
+
+    agent any
+ 
+    stages {
+
+        stage('SCA') {
+
+            steps {
+
+                 script {
+
+                    try {
+
+                        bat 'D:/Snyk/snyk-win.exe code test D:/Snyk/DVWA-master >> D:/Snyk/Report1.txt'
+
+                    } catch (Exception e) {
+
+                        echo "Snyk test failed, but build continues"
+
+                    }
+
+                 }
+
+            }
+
+        }
+
+    }
+
+}
+ 
